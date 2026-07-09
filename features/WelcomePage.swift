a6d26e7929df854516c40585ecef51437cc6a411
Ianna Zambrano <zambranosch0lar@gmail.com>
2026-07-09 12:19:55 -0500

    more to welcome pg

--- features/WelcomePage.swift
+++ features/WelcomePage.swift
@@ -6,8 +6,20 @@
 //
 struct WelcomePage: View {
     var body: some View {
-        Text("Welcome to My App")
-            .font(Font.title)
+        VStack {
+            RoundedRectangle(cornerRadius: 25)
+
+            Text("Welcome to My App")
+                .font(.title)
                 .fontWeight(.semibold)
+                .border(.black, width: 1.5)
+        }
+        .border(.orange, width: 1.5)
+    .padding()
+     .border(.purple, width: 1.5)
+
+        Text("Description text")
+            .font(.title2)
     }
+     .padding()
 }
