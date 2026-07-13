70eb6bc079a7282997f6bd6b09e5f22a46ae50f7
proboy3716 <alexcoro242@gmail.com>
2026-07-13 15:22:01 -0500

    improving code for button by making a list

--- MiiCode 2.swift
+++ MiiCode 2.swift
@@ -4,14 +4,18 @@
 //
 //  Created by DPI Student 004 on 7/7/26.
 //
+var body: some View {
+    List {
+        Text("xbox")
+    Text("playstation")
+        Text("nintendo")
+    }
+}
 
-let Company = "xbox", "playstation", "nintendo"
 let xboxOptions = "xbox series x", "xbox series s", "xboxOne", "xbox360"
 let PlaystationOptions = "ps5", "ps4", "ps3", "psvita", "ps2", "ps1"
 let nintendoOptions = "gamecube", "gameboy", "ds", "3ds", "wii", "switch"
 let otherOptions = "sega", "meta", "atari" 
 button(action: SelectCompany) {
-  Text("Nintendo")
-  Text("Sony")
-  Text("Xbox")
+Text("show list")
 }
