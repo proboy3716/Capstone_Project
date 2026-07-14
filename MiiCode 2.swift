2357d97d1d82c7ff4670251d18b732c5f2005f09
proboy3716 <alexcoro242@gmail.com>
2026-07-14 16:07:01 -0500

    changing code

--- MiiCode 2.swift
+++ MiiCode 2.swift
@@ -4,18 +4,44 @@
 //
 //  Created by DPI Student 004 on 7/7/26.
 //
-var body: some View {
-    List {
-        Text("xbox")
-    Text("playstation")
-        Text("nintendo")
-    }
+struct company: View {
+    case "Xbox"
+    case "Sony"
+    case "Nintendo"
 }
 
-let xboxOptions = "xbox series x", "xbox series s", "xboxOne", "xbox360"
-let PlaystationOptions = "ps5", "ps4", "ps3", "psvita", "ps2", "ps1"
-let nintendoOptions = "gamecube", "gameboy", "ds", "3ds", "wii", "switch"
-let otherOptions = "sega", "meta", "atari" 
+struct XboxOptions: View {
+    case "xbox360"
+    case "xboxOne"
+    case "xboxSeriesX"
+    case "xboxSeriesS"
+    case "xbox360E"
+    case " xbos360Slim"
+    
+}
+struct SonyOptions: View {
+    case "ps5"
+    case "ps4"
+    case "ps3"
+    case "psvita"
+    case "ps2"
+    case "ps1"
+}
+struct NintendoOptions: view {
+    case "wiiU"
+    case "gamecube"
+    case "gameboy", "ds", "3ds", "wii", "switch"
+}
+let otherOptions = "sega", "meta", "atari"
 button(action: SelectCompany) {
 Text("show list")
 }
+Button(action:{
+    .toggle()
+}, label: {
+    Text("select company")
+        .padding()
+        .background(.blue)
+        .cornerRadius(20)
+})
+#Preview
